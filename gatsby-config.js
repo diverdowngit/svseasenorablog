9aaea5bf0fdc9af3774694134996cedcdfd3fda0
Fred <73175901+diverdowngit@users.noreply.github.com>
2023-05-02 11:16:10 +0200

    added css

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-VM8E9LJHKB", // Google 
+          "G-HFY7RJQF04", // Google 
         ],
       
         pluginConfig: {
